ded7b6855e95a211eae3d342d957ce1b496229a2
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2026-07-21 14:03:56 -0300

    fix: fixing spa page configs to vercel

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  appType: 'spa',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    historyApiFallback: true,
+  },
 })
